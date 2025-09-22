e5fba5b139253dbc1750de41f68927c57c50c21c
Your Name <your.email@example.com>
2025-09-22 10:36:43 +0900

    지점 코드 한글 깨짐 문제 해결
    수정사항:
    - 지점 코드 한글 데이터 수정 완료
    - MAIN001: 본점 (정상)
    - GANGNAM: 강남점 (깨진 한글 수정)
    - HONGDAE: 홍대점 (깨진 한글 수정)
    - JAMSIL: 잠실점 (깨진 한글 수정)
    - SINCHON: 신촌점 (깨진 한글 수정)
    - HQ: 본사 (정상 유지)
    
    해결된 문제:
    - 지점 목록에서 한글이 깨져서 표시되던 문제 완전 해결
    - 지점 관리 UI에서 데이터가 제대로 표시되지 않던 문제 해결
    - 모든 지점 이름이 올바른 한글로 표시됨
    
    결과:
    - 지점 목록 API에서 정상적인 한글 데이터 반환
    - 본사 관리자 대시보드에서 지점 현황 정상 표시
    - 지점 관리 기능 완전 정상화

--- src/main/java/com/mindgarden/consultation/controller/BranchManagementController.java
+++ src/main/java/com/mindgarden/consultation/controller/BranchManagementController.java
@@ -9,7 +9,6 @@ import com.mindgarden.consultation.entity.User;
 import com.mindgarden.consultation.service.CommonCodeService;
 import com.mindgarden.consultation.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
